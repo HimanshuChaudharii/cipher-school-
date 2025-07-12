0ca732cab5dc8724b10bc8f94c3adae2b6d3b5ef
HIMANSHU CHAUDHARI <149525377+HimanshuChaudharii@users.noreply.github.com>
2025-07-12 16:45:39 +0530

    Update sol12.java

--- sol12.java
+++ sol12.java
@@ -3,7 +3,7 @@
 	//⁠Declare and initialize variables for your name, age, and favorite programming language. Print them in a formatted sentence.
 	public class Main {
 		public static void main(String[] args) {
-			String name="Anchal Kumari";
+			String name="Himanshu Chaudhari";
 			int age=21;
 			String fav_pl="Java";
 			System.out.println("Hello! "+"My name is " +name +". "+ "I am "+ age+ "old." +" "+ "My favourite Programming Lnaguage is : "+ fav_pl);
